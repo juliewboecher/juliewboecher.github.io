62e0636d6a0defa606d97fa836a9589befc04c98
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-08-07 13:13:39 +0200

    Update Footer component with new email and SVG icons for GitHub and LinkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,9 +11,15 @@ function Footer() {
         <p className="footer-text">© 2026 Julie Wendelboe Bøcher.</p>
 
         <div className="footer-links">
-          <a href="mailto:dinmail@email.com">Email</a>
-          <a href="https://github.com/">GitHub</a>
-          <a href="https://linkedin.com/">LinkedIn</a>
+          <a href="mailto:dinmail@email.com">
+              julie@boecher.dk
+          </a>
+          <a href="https://github.com/">
+            <img className="footer-icon" src={`${import.meta.env.BASE_URL}githvid.svg`} alt="GitHub" />
+          </a>
+          <a href="https://linkedin.com/">
+            <img className="footer-icon" src={`${import.meta.env.BASE_URL}linkedhvid.svg`} alt="LinkedIn" />
+          </a>
         </div>
       </div>
     </footer>
